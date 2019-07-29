572f3021c01d25a41a20f7b190b6aa936cfaa3ad
Jimmy Keesee <jimmy@keesee.net>
2019-07-29 15:56:50 +0800

    added note to geo

--- src/geo.js
+++ src/geo.js
@@ -4,6 +4,8 @@ var volume = require('./calcVolume') // func
 // console.log(area)
 // console.log(volume)
 
+// this is calc area amd calc volume combined
+
 var toExport = {
     areaRectangle: area.rectangle,
     areaCircle: area.circle,
